5ab9235ddd1129bb0710fc22f611a5e15e73bc6c
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-06 22:29:43 +0530

    created all routes components and Switch in not working in react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import App from './App'
+import { BrowserRouter as Router } from "react-router-dom"
+import 'antd/dist/antd.css'
+reactDom.render(
 
-reactDom.render(<App />, document.getElementById('root'));
+    <Router>
+        <App />
+    </Router>,
+    document.getElementById('root')
+);
