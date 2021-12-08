cff287f64c1a02458a5ae7650dd97e1a12ffd40d
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-08 21:21:35 +0530

    created footer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import { Layout, Typography, Space } from 'antd'
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components'
@@ -18,30 +18,27 @@ const App = () => {
             <div className="main" >
                 <Layout>
                     <div className="routes">
-                        <Switch>
-                            <Route exact path='/'>
-                                <Homepage />
-                            </Route>
-                            <Route exact path='/exchanges'>
-                                <Exchanges />
-                            </Route>
-                            <Route exact path='/cryptocurrencies'>
-                                <Cryptocurrencies />
-                            </Route>
-                            <Route exact path='/crypto/:coinId'>
-                                <CryptoDetails />
-                            </Route>
-                            <Route exact path='/news'>
-                                <News />
-                            </Route>
-                        </Switch>
+                        <Routes>
+                            <Route path='/' element={<Homepage />} />
+                            <Route path='/exchanges' element={<Exchanges />} />
+                            <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+                            <Route path='/news' element={<News />} />
+                        </Routes>
                     </div>
                 </Layout>
             </div>
-
-
             <div className="footer" >
+                <Typography.Title level={5} style={{ color: 'white', textAlign = 'center' }} >
+                    Cryptoverse <br />
+                    All right reserved
+                </Typography.Title>
 
+                <Space>
+                    <Link to='/' >Home</Link>
+                    <Link to='/exchanges'>Exchanges</Link>
+                    <Link to='/news'>News</Link>
+                </Space>
             </div>
         </div>
     )
