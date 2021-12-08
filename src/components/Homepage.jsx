b74ee0dfecf9796a1ec74cf6ad55b354c6ef01dc
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-08 21:35:34 +0530

    Started homepage functionality and layout

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import millify from 'millify'
+import { Typography, Row, Col, Statistic } from 'antd'
+import { Link } from 'react-router-dom'
+const { Title } = Typography
+
 
 const Homepage = () => {
     return (
-        <div>
-            Homepage
-        </div>
+        <>
+            <Title level={2} className="Heading" >
+                Global Crypto Stats
+            </Title>
+
+            <Row>
+                <Col span={12}>
+                    <Statistic title="Total Cryptocurrencies" value='5' />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Exchange" value='5' />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Market cap" value='5' />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total 24h Volume" value='5' />
+                </Col>
+                <Col span={12}>
+                    <Statistic title="Total Markets" value='5' />
+                </Col>
+            </Row>
+        </>
     )
 }
 
