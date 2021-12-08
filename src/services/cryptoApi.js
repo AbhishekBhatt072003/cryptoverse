b8058eba35a6126a165782a265a31493baf97ac4
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-08 22:12:19 +0530

    created redux store

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     headers: {
+//         'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//         'x-rapidapi-key': 'bfe382da6amsh08faa74337226a4p152632jsnba24844983d5'
+//     }
+// };
