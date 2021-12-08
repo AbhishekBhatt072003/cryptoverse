b414080334d485ce537a96da6b7258425385ad27
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-08 21:29:18 +0530

    adjusted footer

--- src/App.js
+++ src/App.js
@@ -27,18 +27,18 @@ const App = () => {
                         </Routes>
                     </div>
                 </Layout>
-            </div>
-            <div className="footer" >
-                <Typography.Title level={5} style={{ color: 'white', textAlign = 'center' }} >
-                    Cryptoverse <br />
-                    All right reserved
-                </Typography.Title>
+                <div className="footer" >
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }} >
+                        Cryptoverse <br />
+                        All right reserved
+                    </Typography.Title>
 
-                <Space>
-                    <Link to='/' >Home</Link>
-                    <Link to='/exchanges'>Exchanges</Link>
-                    <Link to='/news'>News</Link>
-                </Space>
+                    <Space>
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
