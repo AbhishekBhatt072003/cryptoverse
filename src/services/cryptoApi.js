21cdfe71050ed494744c088e9a97e8cc95df5cd1
Abhishek Bhatt <abhishekbhatt072003@gmail.com>
2021-12-23 06:11:52 +0530

    created redux store and used redux hooks

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,4 +1,34 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/'
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': 'bfe382da6amsh08faa74337226a4p152632jsnba24844983d5'
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+
+
+export const cryptoApi = createApi(
+    {
+        reducerPath: 'cryptoApi',
+        baseQuery: fetchBaseQuery({ baseUrl }),
+        endpoints: (builder) => ({
+            getCryptos: builder.query({
+                query: () => createRequest('/coins')
+            })
+        })
+    }
+);
+
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
+
+
+
 
 
 // var options = {
